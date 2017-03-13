edb258740a0690e19996c812ec385c842efdea2c
Bernard Allotey <benard.allotey4@yahoo.com>
2017-03-13 16:10:14 -0500

    Initial Commit

---
+++ ConsoleRPG/Move.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG
+{
+    class Move
+    {
+        private string moveName;
+        private int manaCost;
+        private int baseDamage;
+        private double damageScale;
+
+        public Move(string _moveName, int _manaCost, int _baseDamage, double _damageScale)
+        {
+            this.moveName = _moveName;
+            this.manaCost = _manaCost;
+            this.baseDamage = _baseDamage;
+            this.damageScale = _damageScale;
+        }
+
+        public int useMove(double addedDamage)
+        {
+            int damage;
+            double scaledDamage = this.damageScale * addedDamage;
+            damage = baseDamage + (int)Math.Round(scaledDamage);
+            return damage;
+        }
+
+        public int getBaseDamage()
+        {
+            return baseDamage;
+        }
+
+        public void setBaseDamage(int _baseDamage)
+        {
+            this.baseDamage = _baseDamage;
+        }
+
+        public double getDamageScale()
+        {
+            return damageScale;
+        }
+
+        public void setDamageScale(int _damageScale)
+        {
+            damageScale = _damageScale;
+        }
+    }
+}
